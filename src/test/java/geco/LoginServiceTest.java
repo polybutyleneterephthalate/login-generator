f0f9e05fe6436117c05ef0a123dc8ca25aae7833
polybutyleneterephthalate <stefano.matthia@gmail.com>
2017-11-21 11:36:18 +0100

    #1 create junit test class for LoginService

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -1,5 +1,24 @@
+package geco;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LoginServiceTest {
+    @Test
+    public void loginExists() throws Exception {
+    }
+
+    @Test
+    public void addLogin() throws Exception {
+    }
+
+    @Test
+    public void findAllLoginsStartingWith() throws Exception {
+    }
+
+    @Test
+    public void findAllLogins() throws Exception {
+    }
 
 }
